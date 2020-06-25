094e81d10e90aa8d8615ea97230f4cfc28f70225
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-25 18:34:18 -0500

    creates a read me properly

--- index.js
+++ index.js
@@ -5,70 +5,72 @@
 //require inquire
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require("utils/generateMarkdown.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-const questions =[
+const questions = [
     {
-      type: "input",
-      message: "What is your title?",
-      name: "title",
+        type: "input",
+        message: "What is your title?",
+        name: "title",
     },
     {
-      type: "input",
-      message: "What is your description?",
-      name: "description",
+        type: "input",
+        message: "What is your description?",
+        name: "description",
     },
     {
-      type: "input",
-      message: "What are the steps required to install your project?",
-      name: "installation",
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "installation",
     },
     {
         type: "input",
         message: "What are your instructions for use?",
         name: "usage",
-      },
-      {
+    },
+    {
         type: "input",
         message: "Who are your collaborators?",
         name: "credits",
-      },
-      {
+    },
+    {
         type: "list",
         message: "What is your licenses?",
         name: "license",
-        choices: ["MIT", "GPL","Apache License","Mozilla Public License"],
-      },
-      {
+        choices: ["MIT", "GPL", "Apache License", "Mozilla Public License"],
+    },
+    {
         type: "input",
         message: "What are your contribution guidelines?",
         name: "contributing",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What tests were ran?",
         name: "tests",
-      },
-         {
+    },
+    {
         type: "input",
         message: "What email should questions about your project be sent to?",
         name: "questions",
-      },
-      
-  ]
-  function init(){
-inquirer.prompt (questions)
-  .then(function (answer) {
-      console.log(answer);
+    },
 
-      var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-    fs.writeFile("sampleReadMe.md", generateMarkdown(answer), function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("Success!");
-    });
-  });
+]
+function init() {
+    inquirer.prompt(questions)
+        .then(function (answer) {
+            console.log(answer);
+
+            //   var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+            fs.writeFile("sampleReadMe.md", generateMarkdown(answer), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("Success!");
+            });
+        })};
+        //function call to initlialize 
+        init();
 
 // array of questions for user
 
